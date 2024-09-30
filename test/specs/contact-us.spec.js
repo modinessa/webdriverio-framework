b5d35edf516b58c9e3ff944e6c5933efe09cfeb2
Inessa Modina <modina.inessa@gmail.com>
2024-09-30 12:43:18 -0700

    First test challenge (22)

--- test/specs/contact-us.spec.js
+++ test/specs/contact-us.spec.js
@@ -12,22 +12,41 @@ describe('webdriveruniversity - contact us page', () => {
         const emailAddress = await $('//*[@name="email"]');
         //message
         const message = await $('//*[@name="message"]');
-
         //submit button
+        const submitButton = await $('//input[@value="SUBMIT"]');
         
         await firstName.setValue("Joe");
         await lastName.setValue("Smith");
         await emailAddress.setValue("js@gmail.com");
         await message.setValue("Hi! Nice to meet you!");
+        await submitButton.click();
+
+        //Success message
+        const successfulSubmitionHeader = $('#contact_reply > h1');
+        await expect(successfulSubmitionHeader).toHaveText('Thank You for your Message!');
         await browser.pause(5000);
         
     });
     
-    it.skip('invalid submition - dont submit all information', () => {
+    it.only('invalid submition - dont submit all information', async() => {
+        await browser.url('/Contact-Us/contactus.html')
         //first name
+        const firstName = await $('//*[@name="first_name"]');
+        await firstName.setValue("Joe");
         //last name
-        //email address
+        const lastName = await $('//*[@name="last_name"]');
+        await lastName.setValue("Smith");
         //message
+        const message = await $('//*[@name="message"]');
+        await message.setValue("Hi! Nice to meet you!");
+        
         //submit button
+        const submitButton = await $('//input[@value="SUBMIT"]');
+        await submitButton.click();
+        
+
+        //Fail submition
+        const errorMessage = $('body');
+        await expect(errorMessage).toHaveTextContaining(['Error: all fields are required', 'Error: Invalid email address']);
     });
 });
